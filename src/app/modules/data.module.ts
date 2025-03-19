05e8a4321a8fc5db4cf151534a0efb766734f31e
Sergio Cortés <s.cortes@montechelo.com.co>
2025-03-19 00:54:46 -0500

    19-03-25-BuildingStructure * The Product's CRUD was made using Hexagonal Architecture. * The TSConfig.JSON was configured for paths.

---
+++ src/app/modules/data.module.ts
@@ -0,0 +1,5 @@
+import { ProductModule } from "./products/product.module";
+
+export const DataModule = [
+  ProductModule
+];
---
+++ src/app/modules/products/ui/pages/products/products.component.ts
@@ -0,0 +1,27 @@
+import { Component, inject } from '@angular/core';
+
+// Project
+import { ProductUiService } from '@products/ui/services/product-ui.service';
+
+@Component({
+  selector: 'app-products',
+  standalone: true,
+  imports: [],
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.css'
+})
+export class ProductsComponent {
+  service: ProductUiService = inject(ProductUiService);
+
+  constructor() {
+    this.index();
+  }
+
+  index() {
+    this.service.index().subscribe({
+      next: (response) => {
+        console.log(response);
+      }
+    });
+  }
+}
